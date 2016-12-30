44c91b6f1e9921f15b2e2b997b48d17b51972bfe
Artsy <craigspaeth@gmail.com>
2016-12-30 00:03:43 -0500

    broke up the modules

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const joi = require('joi')
 const joiql = require('joiql')
 const pluralize = require('pluralize')
-const { camelCase, mapValues } = require('lodash')
-const { db, connect } = require('./lib/db')
+const { camelCase } = require('lodash')
+const db = require('./lib/db')
 const wrappedType = require('./lib/wrapped-type')
 const middlewares = require('./lib/middlewares')
+const attrsToSchemas = require('./lib/attrs-to-schemas')
 
-// Export types.
+// Export wrapped Joi type APIs
 module.exports.id = wrappedType('id')
 module.exports.string = wrappedType('string')
 module.exports.boolean = wrappedType('boolean')
@@ -14,19 +14,19 @@ module.exports.number = wrappedType('number')
 module.exports.object = wrappedType('object')
 module.exports.date = wrappedType('date')
 
-// Connect and export our Mongo database.
-module.exports.db = db
-module.exports.connect = connect
+// Connect and export our Mongo database
+module.exports.db = db.db
+module.exports.connect = db.connect
 
-// Given a list of models turn it into a GraphQL.js schema object.
+// Given a list of models turn it into a GraphQL.js schema object
 module.exports.graphqlize = (...models) => {
   const joiqlSchema = { query: {}, mutation: {} }
   models.forEach((model) => {
-    joiqlSchema.mutation[`create${model.singular}`] = model.schema().create
-    joiqlSchema.query[camelCase(model.singular)] = model.schema().read
-    joiqlSchema.mutation[`update${model.singular}`] = model.schema().update
-    joiqlSchema.mutation[`delete${model.singular}`] = model.schema().delete
-    joiqlSchema.query[camelCase(model.plural)] = model.schema().list
+    joiqlSchema.mutation[`create${model.singular}`] = model.schemas.create
+    joiqlSchema.query[camelCase(model.singular)] = model.schemas.read
+    joiqlSchema.mutation[`update${model.singular}`] = model.schemas.update
+    joiqlSchema.mutation[`delete${model.singular}`] = model.schemas.delete
+    joiqlSchema.query[camelCase(model.plural)] = model.schemas.list
   })
   return joiql(joiqlSchema)
 }
@@ -34,7 +34,7 @@ module.exports.graphqlize = (...models) => {
 // Factory function that creates a model object.
 //
 // Models are a collection of Joi schemas for CRUDL operations, contained in
-// `model.schema().create` for instance. Models also provide a `model.on`
+// `model.schemas.create` for instance. Models also provide a `model.on`
 // function for hooking Koa-like middleware into CRUDL operation.
 //
 // e.g.
@@ -46,62 +46,15 @@ module.exports.graphqlize = (...models) => {
 // })
 // ```
 //
-module.exports.model = (singular, initAttrs) => {
-  let attributes
+module.exports.model = (singular, attrs) => {
   const plural = pluralize(singular)
-  const schema = { create: {}, read: {}, update: {}, delete: {}, list: {} }
-  const { resolver, on } = middlewares(() => db[camelCase(plural)])
-
-  // Converts an object of wrapped Joi types into individual Joi schemas used
-  // to validate each CRUDL opeartion's args.
-  const attrs = (attrs) => {
-    if (!attrs) return attributes
-    if (!attrs._id) {
-      attrs._id = wrappedType('id')()
-      attrs._id.on('delete update').required()
-    }
-    const schemaObj = (method = 'all') =>
-      mapValues(attrs, (attr) => attr.schema(method))
-    const args = {
-      create: schemaObj('create'),
-      read: schemaObj('read'),
-      update: schemaObj('update'),
-      delete: schemaObj('delete'),
-      list: schemaObj('list')
-    }
-    const fields = joi.object(schemaObj())
-    attributes = wrappedType('object')(schemaObj())
-    schema.create = fields.meta({
-      args: args.create,
-      resolve: resolver('create')
-    })
-    schema.read = fields.meta({
-      args: args.read,
-      resolve: resolver('read')
-    })
-    schema.update = fields.meta({
-      args: args.update,
-      resolve: resolver('update')
-    })
-    schema.delete = fields.meta({
-      args: args.delete,
-      resolve: resolver('delete')
-    })
-    schema.list = joi.array().items(fields).meta({
-      args: args.list,
-      resolve: resolver('list')
-    })
-  }
-
-  // Convenience for passing attrs as a second argument
-  if (initAttrs) attrs(initAttrs)
-
-  // Compose a model object from the Joi schemas and methods
+  const { resolver, on } = middlewares(camelCase(plural))
+  const schemas = attrsToSchemas(attrs, resolver)
   return {
     singular,
     plural,
     attrs,
-    schema: () => schema,
+    schemas,
     on
   }
 }
