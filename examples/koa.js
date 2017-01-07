f6a1a412ca1d0c5abe45dba0c96316a101621867
Artsy <craigspaeth@gmail.com>
2017-01-07 18:24:02 -0500

    bump version

--- examples/koa.js
+++ examples/koa.js
@@ -7,6 +7,6 @@ connect('mongodb://localhost:27017/test')
 
 const app = new Koa()
 
-app.use(koaize('/', user))
+app.use(koaize('/api', user))
 
 app.listen(3000, () => console.log('listening on 3000'))
